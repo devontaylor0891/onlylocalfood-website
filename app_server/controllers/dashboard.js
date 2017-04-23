43ad3af2f46243271979333dad1ee5fae785054c
Devon Taylor <devontaylor0891@gmail.com>
2017-04-23 11:56:15 -0600

    Clean up colors, fix bugs, move info into controllers.

--- app_server/controllers/dashboard.js
+++ app_server/controllers/dashboard.js
@@ -1,18 +1,7 @@
 
 /* GET dashboard page */
 module.exports.dashboard = function (req, res, next) {
-	res.render('dashboard', {
-		title: 'Your Dashboard',
-		orders: [
-			{
-				firstName: 'Bob',
-				city: 'Moosomin'
-			}, {
-				firstName: 'Betty',
-				city: 'Wapella'
-			}
-		]
-	})
+	res.render('dashboard')
 };
 
 /* GET consumer dashboard page */
